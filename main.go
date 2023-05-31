d5124ab2bb4caf1ebd9599b886e655d68deae4fc
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-05-31 18:24:56 +0300

    create functions to make requests

--- main.go
+++ main.go
@@ -1,23 +1,55 @@
 package main
 
 import (
+	"GoNews/news"
+	"bytes"
 	"fmt"
 	"html/template"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
-	"GoNews/news"
 
 	"github.com/joho/godotenv"
 )
 
 
 
-var tpl = template.Must(template.ParseFiles("index.html"))
+var tpl = template.Must(template.ParseFiles("templates/index.html"))
+
+type Search struct {
+	Query string
+	NextPage int
+	TotalPages int
+	Results *news.Results
+}
+
+func (s *Search) IsLastPage() bool {
+	return s.NextPage >= s.TotalPages
+}
+
+func (s *Search) CurrentPage() {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+	return s.NextPage - 1
+}
+
+func (s *Search) PreviousPage() int {
+	return s.CurrentPage() - 1
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	buf := &bytes.Buffer{}
+	err := tpl.Execute(buf, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	buf.WriteTo(w)
 	tpl.Execute(w,nil)
 }
 
@@ -34,7 +66,37 @@ func searchHandler(newsapi *news.Client) http.HandlerFunc {
 		if page == "" {
 			page = "1"
 		}
-        fmt.Println("Search Query is:", searchQuery)
+
+		results, err := newsapi.FetchEverything(searchQuery,page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		nextPage, err := strconv.Atoi(page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		search := &Search{
+			Query: searchQuery,
+			NextPage: nextPage,
+			TotalPages: int(math.Ceil(float64(results.TotalResults) / float64(newsapi.PageSize))),
+			Results: results,
+		}
+
+		if ok := !search.IsLastPage(); ok{
+			search.NextPage++
+		}
+
+		buf := &bytes.Buffer{}
+		err = tpl.Execute(buf,search)
+		if err != nil{
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+			return
+		}
+		buf.WriteTo(w)
 	    fmt.Println("Page is:", page)
 	}
 }
